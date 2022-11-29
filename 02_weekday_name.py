19b97736435807a4b38471f8a4531a590d4677ee
Nico Martinucci <domenico.martinucci@gmail.com>
2022-11-28 16:42:15 -0800

    updates

--- 02_weekday_name.py
+++ 02_weekday_name.py
@@ -12,6 +12,14 @@ def weekday_name(day_of_week):
         >>> weekday_name(9)
         >>> weekday_name(0)
     """
-    weekdays = {1:"Sunday", 2:"Monday", 3:"Tuesday", 4:"Wednesday", 5:"Thursday", 6:"Friday", 7:"Saturday"}
+    WEEKDAYS = {
+        1:"Sunday", 
+        2:"Monday", 
+        3:"Tuesday", 
+        4:"Wednesday", 
+        5:"Thursday", 
+        6:"Friday", 
+        7:"Saturday"
+    }
 
     return weekdays.get(day_of_week, None)
--- 07_multiple_letter_count.py
+++ 07_multiple_letter_count.py
@@ -9,3 +9,5 @@ def multiple_letter_count(phrase):
     """
 
     return {letter : phrase.count(letter) for letter in phrase}
+
+    # how can we do this with a .get() method call?
